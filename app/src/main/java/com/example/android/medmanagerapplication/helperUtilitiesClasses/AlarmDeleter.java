7101bb64d6f0f2754a064d46e67d5c63b9d7a992
george udosen <datameshprojects@gmail.com>
2018-04-18 23:27:11 +0100

    updated Alarmchecker again

--- app/src/main/java/com/example/android/medmanagerapplication/helperUtilitiesClasses/AlarmDeleter.java
+++ app/src/main/java/com/example/android/medmanagerapplication/helperUtilitiesClasses/AlarmDeleter.java
@@ -6,13 +6,8 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.Nullable;
-import android.util.Log;
 
 public class AlarmDeleter extends IntentService {
-
-    private static final String TAG = AlarmDeleter.class.getSimpleName();
-
-    private Context mContext;
 
     /**
      * Creates an IntentService.  Invoked by your subclass's constructor.
@@ -28,11 +23,9 @@ public class AlarmDeleter extends IntentService {
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
 
-        Log.v(TAG, "IntentService called");
 
         if (intent != null) {
             long id = intent.getLongExtra("drugId", 1);
-            Log.v(TAG, "do loop in receiver called from AlarmDeleter class:" + id);
 
             try {
                 Intent notificationIntent = new Intent(getApplicationContext(), DrugReceiver.class);
